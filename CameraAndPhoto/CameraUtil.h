b8260e589270a2a1e7ef9068a1c0d7a13a31faea
王书涛 <wststar@126.com>
2014-04-12 10:33:58 +0800

    添加注释

--- CameraAndPhoto/CameraUtil.h
+++ CameraAndPhoto/CameraUtil.h
@@ -12,18 +12,62 @@
 
 @interface CameraUtil : NSObject
 
+/**
+ *	@brief	摄头是否有效
+ *
+ *	@return
+ */
 +(BOOL)isCameraAvailable;
 
+
+/**
+ *	@brief	前置摄头是否有效
+ *
+ *	@return
+ */
 +(BOOL)isFrontCameraAvailable;
 
+
+/**
+ *	@brief	后置摄头是否有效
+ *
+ *	@return
+ */
 +(BOOL)isRearCameraAvailable;
 
+
+
+/**
+ *	@brief	前置摄头是否装配了闪光灯
+ *
+ *	@return
+ */
 +(BOOL) isFlashAvailableOnFrontCamera;
 
+
+
+/**
+ *	@brief	后置摄头是否装配了闪光灯
+ *
+ *	@return
+ */
 +(BOOL) isFlashAvailableOnRearCamera;
 
+
+/**
+ *	@brief	摄头是否支持拍摄视频
+ *
+ *	@return
+ */
 +(BOOL)doesCameraSupportShootingVideos;
 
+
+/**
+ *	@brief 摄头是否支持照相
+ *
+ *	@return
+ */
 +(BOOL) doesCameraSupportTakingPhotos;
+
 
 @end
